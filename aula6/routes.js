da8a375cbdc15b81dfe4710ba4e0f4cbb5d201a7
Artur Cavalcante de Jesus <56032621+arturcavalcantej@users.noreply.github.com>
2022-10-03 14:22:04 -0300

    feat: initial commit

---
+++ aula6/routes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const route = express.Router()
+const homeController = require('./homeController')
+// rotas da home
+route.get('/',homeController.paginaInicial);
+route.post('/',homeController.trataPost);
+module.exports = route
